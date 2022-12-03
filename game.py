2640770a1565f4d3104386cc4bdd7a5c1fa350be
James Beck <jamesstuartbeck@gmail.com>
2022-12-03 13:00:13 -0500

    Added game.py

---
+++ game.py
@@ -0,0 +1,22 @@
+from pystage.en import Stage
+
+def intro(sprite):
+    for i in range(4):
+        sprite.move(10)
+        sprite.wait(1)
+        sprite.think("Loading...")
+        sprite.turn_left(90)
+        sprite.wait(1)
+        sprite.think("")
+    sprite.say("Hello!")
+    sprite.wait(3)
+    sprite.say("")
+
+def game():
+    stage = Stage()
+    stage.add_backdrop("grid")
+    zombie = stage.add_a_sprite()
+    zombie.when_program_starts(intro)
+    stage.play()
+
+game()
